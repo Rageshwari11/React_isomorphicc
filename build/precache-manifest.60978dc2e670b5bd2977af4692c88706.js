a0570cbd308c6d9a045fa2c0d81d2464c1dbd1bf
Rageshwari <singhrageshwari1108@gmail.com>
2020-10-26 22:25:23 +0530

    finalcodee

---
+++ build/precache-manifest.60978dc2e670b5bd2977af4692c88706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa1803b3a3ab909402208628a874115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3888242f14270302c6a0",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "0ab1feb5a812d57d8975",
+    "url": "/static/css/main.48b76d01.chunk.css"
+  },
+  {
+    "revision": "3888242f14270302c6a0",
+    "url": "/static/js/2.5f635476.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f635476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab1feb5a812d57d8975",
+    "url": "/static/js/main.e1a0642d.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
